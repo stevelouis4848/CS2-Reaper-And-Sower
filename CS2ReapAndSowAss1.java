e2d88289dc4cde95d105c574842c046d91fc93c6
stevelouis4848 <33739350+stevelouis4848@users.noreply.github.com>
2018-09-08 17:10:09 -0400

    final with most debug outputs removed

--- CS2ReapAndSowAss1.java
+++ CS2ReapAndSowAss1.java
@@ -3,8 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package cs2.reapandsow.ass.pkg1;
-
 
 /**
  *
@@ -21,14 +19,14 @@ public class CS2ReapAndSowAss1 {
     public static int n;
     public static int m;
     public static int input[][];
-    public static int pastSolutionBinaryRow[];
-    public static int pastSolutionBinaryCollum[];
+    public static int pastSolutionsBinaryRow[];
+    public static int pastSolutionsBinaryCollum[];
     
         
     public static void main(String[] args) {
         
         // TODO code application logic here
-        //TO read the inouts into the program.
+        //TO read the inputs into the program.
         
         System.out.print("Please enter inputs");
         Scanner sc = new Scanner(System.in);
@@ -38,15 +36,17 @@ public class CS2ReapAndSowAss1 {
         
         input = new int[n][m];
         
-        pastSolutionBinaryRow = new int[n];
-        pastSolutionBinaryCollum = new int[m];
-                    
+        pastSolutionsBinaryRow = new int[n];
+        pastSolutionsBinaryCollum = new int[m];
+                  
+        //  Reads in the preset values for the grid
         for(int i = 0; i < n; i++){           
             for(int j = 0; j < m; j++){
                 
                 input[i][j] = sc.nextInt();
             }
         }
+        
         
         if(solveFunction(0, 0) == true){
             
@@ -59,69 +59,80 @@ public class CS2ReapAndSowAss1 {
               
     public static boolean solveFunction(int row, int collum){
 
-       // base cases
-       
-               
+       // When the end of a row is reach values are set to go to next row            
         if(collum == m){
-           
             row = row + 1;
             collum = 0;
         }
-            
+          
+        //  A solution is found if we past the last row
         if(row > n - 1){
             
-            printOutput(n, m, input, pastSolutionBinaryRow, pastSolutionBinaryCollum);
+            printOutput(n, m, input, pastSolutionsBinaryRow, pastSolutionsBinaryCollum);
             return true;
         }
-            
-         if(input[row][collum] != -1  ){
+          
+        /*  Checks if the current square passes the tests if value already preset
+            if it passes we move to the next square.Otherwise we return false to
+            backtrack to a previous solutionn
+        */
+        if(input[row][collum] != -1  ){
              
-            if(!isValid(input, pastSolutionBinaryRow, pastSolutionBinaryCollum, row, collum))
+            if(!isValid(input, pastSolutionsBinaryRow, pastSolutionsBinaryCollum, row, collum))
                      return false;
                  
-            if(!isValid(input, pastSolutionBinaryRow, pastSolutionBinaryCollum, row, collum))
+            if(!isValid(input, pastSolutionsBinaryRow, pastSolutionsBinaryCollum, row, collum))
                     return false;
                                        
            return solveFunction(row, collum + 1);
         }
                
-        //recursion and backtracking
+        /* Inserts and checks the validity of the values 1 and 0 in the current 
+           position in the grid */
         
         for(int i = 1; i >= 0; i--){
             
             input[row][collum] = i;
             
-            printOutput(n, m, input, pastSolutionBinaryRow, pastSolutionBinaryCollum);
+           // printOutput(n, m, input, pastSolutionsBinaryRow, pastSolutionsBinaryCollum);
             
-            if(isValid(input, pastSolutionBinaryRow, pastSolutionBinaryCollum, row, collum)){
+            if(isValid(input, pastSolutionsBinaryRow, pastSolutionsBinaryCollum, row, collum)){
                 
                 if(solveFunction(row, collum + 1)){
                     return true;
                 }
             }         
-        }        
+        }   
+        
+        /* when neither 1 or 0 worked, the value of the current position is reset
+           to -1 and we return false in order to backtrack to a previous move. */
         input[row][collum] = -1; 
-       return false;
+        
+        return false;
     }
     
-    public static boolean isValid(int input[][],int pastSolutionBinaryRow[], 
-            int  pastSolutionBinaryCollum[], int row, int collum){
+    /*
+        All the tests are called and evaluate in this function,if any test fails 
+        then this function returns false.
+    */
+    public static boolean isValid(int input[][],int pastSolutionsBinaryRow[], 
+            int  pastSolutionsBinaryCollum[], int row, int collum){
         
         if(!checkCollumConti(input, row, collum) || !checkRowConti(input, row, collum) 
                              || !checkRowMax(input, row, collum, m/2) || !checkCollumMax(input, row, collum, n/2)
-                             || !checkCollumPastSolutions(pastSolutionBinaryCollum,input,row ,collum)
-                             || !checkRowPastSolutions(pastSolutionBinaryRow,input,row ,collum)){
-            
+                             || !checkCollumPastSolutions(pastSolutionsBinaryCollum,input,row ,collum)
+                             || !checkRowPastSolutions(pastSolutionsBinaryRow,input,row ,collum)){         
             return false;
-        }
-        
+        }            
         return true;
     }
         
+    // Checks if a row has 3 or more squares equal to 1 in a row.        
     public static boolean checkRowConti(int input[][], int currentRow, int currentCollum){
        
         int inUsedRow;
        
+        // No need to check more then three in a row before index 2 of that row.
         if(currentCollum > 1){
            
             inUsedRow = input[currentRow][currentCollum];
@@ -129,7 +140,6 @@ public class CS2ReapAndSowAss1 {
             if(input[currentRow][currentCollum - 1] == inUsedRow && 
                     input[currentRow][currentCollum - 2] == inUsedRow){
                 
-                System.out.printf("CheckRowConti false Row:%d Collum:%d\n", currentRow, currentCollum);
                 return false;
             }
         }
@@ -137,10 +147,12 @@ public class CS2ReapAndSowAss1 {
         return true;     
    }
     
+    // Checks if a collum has 3 or more squares equal to 1 in a row.        
     public static boolean checkCollumConti(int input[][],int currentRow, int currentCollum){
        
         int inUsedCollum;
-   
+        
+        // No need to check more then three in a collum before index 2 of that collum
         if(currentRow > 1){
            
             inUsedCollum = input[currentRow][currentCollum];
@@ -148,13 +160,16 @@ public class CS2ReapAndSowAss1 {
             if(input[currentRow - 1][currentCollum ] == inUsedCollum && 
                     input[currentRow - 2][currentCollum] == inUsedCollum){
                 
-                System.out.printf("CheckCollumConti false Row:%d Collum:%d\n", currentRow, currentCollum);
                 return false;
             }
         }
         return true;    
    }
     
+     /*
+        Checks if a row has passed the max ammount of activated squares equal
+        to 1.
+    */
     public static boolean checkRowMax(int input[][], int currentRow, int currentCollum, int rowMax){
          
         int inUsedRow = 0;
@@ -162,28 +177,24 @@ public class CS2ReapAndSowAss1 {
         for(int i = 0; i < m; i++){
          
             if(input[currentRow][i] == 1){
-             
                 inUsedRow++;             
             }
         }
          
-        //System.out.printf("In used Rowmax %d\n",inUsedRow);
         if(inUsedRow > rowMax){
-
-            //System.out.printf("Max in row %d: row %d, collum %d\n", currentRow+1, currentRow+1, currentCollum+1);
-            
             return false;
         }
 
         else if((currentCollum == m - 1) && (inUsedRow < rowMax) ){
-            
-           System.out.printf("CheckRowMax false Row:%d Collum:%d\n", currentRow, currentCollum);
             return false;
-        }
-   
+        }       
         return true;
     }
      
+     /*
+        Checks if a collum has passed the max ammount of activated squares equal
+        to 1.
+    */
     public static boolean checkCollumMax(int input[][],int currentRow, int currentCollum, int collumMax){
       
         int inUsedCollum = 0;
@@ -191,76 +202,78 @@ public class CS2ReapAndSowAss1 {
         for(int i = 0; i < n; i++){
 
             if(input[i][currentCollum] == 1){
-
-                inUsedCollum++;             
+               inUsedCollum++;             
             }
         }
         
-       // System.out.printf("In used collumMax %d\n", inUsedCollum);
-        
         if(inUsedCollum > collumMax){
-
-           System.out.printf("CheckCollumMax false Row:%d Collum:%d\n", currentRow, currentCollum);
-
             return false;
         }
+        
+        else if((currentRow == n - 1) && (inUsedCollum < collumMax)){
+            return false;
+        }      
         return true;
     }
     
-    public static boolean checkRowPastSolutions(int pastSolutionBinaryRow[], int input[][],int currentRow,int currentCollum){
+    /*
+        At the End of solved row the state of the row is stored as unique binary 
+        number to compare against furure and past row solutions to avoid duplcate row states.
+    */
+    public static boolean checkRowPastSolutions(int pastSolutionsBinaryRow[], int input[][],int currentRow,int currentCollum){
     
         int currentRowBinary = 0;
-                   
+        
+        // Solutions for rows are only tore and compared at the end of the rows.
         if(currentCollum == m - 1 ){
             
             for(int i = 0; i < m; i++){
                 
                 currentRowBinary += input[currentRow][i] * Math.pow(2, i); 
             }
-           // System.out.printf("Row: %d after math Binary: %d\n", currentCollum, pastSolutionBinaryRow[currentCollum]);
-            
+         
             for(int i = 0; i < currentRow; i++ ){
                 
-                if(pastSolutionBinaryRow[i] == currentRowBinary){
-                    System.out.printf("CheckRowPastSolutions false Row:%d Collum:%d\n", currentRow, currentCollum);
+                if(pastSolutionsBinaryRow[i] == currentRowBinary){
                     return false;
                 }              
             }
          
-             pastSolutionBinaryRow[currentRow] = currentRowBinary;  
-             System.out.printf("Row: %d Binary : %d\n", currentRow, pastSolutionBinaryRow[currentRow]);        
+             pastSolutionsBinaryRow[currentRow] = currentRowBinary;          
         }
         return true;
     }
     
-    public static boolean checkCollumPastSolutions(int pastSolutionBinaryCollum[], int input[][],int currentRow, int currentCollum){
+    /*
+        At the End of solved collum the state of the collum is stored as unique binary 
+        number to compare against furure and past collum solutions to avoid duplcate row states.
+    */
+    public static boolean checkCollumPastSolutions(int pastSolutionsBinaryCollum[], int input[][],int currentRow, int currentCollum){
     
         int currentCollumBinary = 0;
-              
+         
+        // Solutions for collums are only tore and compared at the end of the collums.
          if(currentRow == n - 1){
             
             for(int i = 0; i < n; i++){
-                
-                 currentCollumBinary += input[i][currentCollum] * Math.pow(2, i);
-                 
+                currentCollumBinary += input[i][currentCollum] * Math.pow(2, i);                
             }
-           // System.out.printf("Collum: %d After math Binary : %d\n", currentCollum, pastSolutionBinaryCollum[currentCollum]);
+         
             for(int i = 0; i < currentCollum; i++ ){
                 
-                if(pastSolutionBinaryCollum[i] == currentCollumBinary){
-                    System.out.printf("CheckCollumPastSolutions false Row:%d Collum:%d\n", currentRow, currentCollum);
+                if(pastSolutionsBinaryCollum[i] == currentCollumBinary){
                     return false;
                 }              
             }         
-             pastSolutionBinaryCollum[currentCollum] = currentCollumBinary; 
-             System.out.printf("Collum: %d Binary : %d\n", currentCollum, pastSolutionBinaryCollum[currentCollum]);
-             
+             pastSolutionsBinaryCollum[currentCollum] = currentCollumBinary;  
         }
      return true;
     }
     
+    // Prints thse current state of the grid to the screen.
+   
     public static void printOutput(int n, int m, int output[][], 
-            int  pastSolutionBinaryRow[], int pastSolutionBinaryCollum[] ){
+            int  pastSolutionsBinaryRow[], int pastSolutionsBinaryCollum[] ){
         
        int squaresWorked = 0;
        
@@ -279,19 +292,7 @@ public class CS2ReapAndSowAss1 {
             }
             System.out.print("\n");
         }
-        
-        System.out.print("Row Binary Solutions:\n");
-        for(int i = 0; i < n ; i++){
-            
-            System.out.printf("%d ", pastSolutionBinaryRow[i]);
-        }
-        
-        System.out.print("\nCollum Binary Solutions:\n");
-        for(int i = 0; i < m ; i++){
-            
-            System.out.printf("%d ", pastSolutionBinaryCollum[i]);
-        }
-        
-        System.out.printf("\nTotal squares worked %d / %d\n", squaresWorked, n*m);      
+       
+        System.out.printf("Total squares worked %d / %d\n", squaresWorked, n*m);      
     }
 }
